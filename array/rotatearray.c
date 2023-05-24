8759c92386661cacc14b6ee5b6fc56cded5c66a1
CODER <125824567+DEVIL001-CODER@users.noreply.github.com>
2023-05-24 22:56:49 +0530

    Create rotatearray.c
    5671234

---
+++ array/rotatearray.c
@@ -0,0 +1,22 @@
+//rotate a array
+#include<stdio.h>
+void reverse(int arr[],int s,int e){
+    for(int i=s,j=e;i<j;i++,j--){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    return;
+}
+int main(){
+    int arr[7]={1,2,3,4,5,6,7};
+    int k=3;
+    int n=7;
+    k=k%n;
+    reverse(arr,0,n-1);
+    reverse(arr,0,k-1);
+    reverse(arr,k,n-1);
+    for(int i=0;i<=6;i++){
+        printf("%d",arr[i]);
+    }
+}
